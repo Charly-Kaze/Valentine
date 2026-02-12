1f8ed74a8adfdbaddacfb4d4c028137758b0208d
Charly-Kaze <charlykouemeko@gmail.com>
2026-02-12 11:04:25 +0100

    Correct the gender agreement in title text

--- appaurore.py
+++ appaurore.py
@@ -77,7 +77,7 @@ else:
     st.balloons()
     st.snow()
     st.balloons() # Double dose de ballons !
-    st.title("YAY ! J'en étais sûr ! ❤️🎈")
+    st.title("YAY ! J'en étais sûre ! ❤️🎈")
     
     try:
         st.image("Aurore.jpeg", width=500)
